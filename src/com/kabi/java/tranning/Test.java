2dd7d3da2a459d95cc56b97b29514431ca6c8d6a
kabi <kabi@kabi-PC>
2015-07-18 14:56:47 +0545

    b hbjj m

--- src/com/kabi/java/tranning/Test.java
+++ src/com/kabi/java/tranning/Test.java
@@ -2,6 +2,6 @@ package com.kabi.java.tranning;
 
 public class Test {
 	 public static void main (String[] args){
-	 System.out.println("Hello world");
+	 System.out.println("Hello world fu");
 }
 }
